207f73aff9a4c3737b080e85a47264dfe3d0ae82
Kouta Nikkuni <kota.nikkuni@linksports.co.jp>
2023-11-07 20:34:54 +0900

    add gql

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {
  CardStyleInterpolators,
  createStackNavigator,
 } from "@react-navigation/stack";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import MemoListScreen from "./src/onBoading/views/MemoListScreen";
 import MemoDetailScreen from "./src/onBoading/views/MemoDetailScreen";
@@ -20,43 +21,50 @@ import { resetIndex } from "./src/redux/modules/counterIndex";
 const Stack = createStackNavigator();
 
 export default function App() {
+ const apolloClient = new ApolloClient({
+  uri: "http://localhost:8083/query/",
+  cache: new InMemoryCache(),
+ });
+
  return (
-  <Provider store={store}>
-   <NavigationContainer>
-    <Stack.Navigator
-     initialRouteName="SignUp"
-     screenOptions={{
-      headerStyle: { backgroundColor: "#dffad4" },
-      headerTitleStyle: { color: "#000000" },
-      headerTitle: i18n.t("headerTitle"),
-      headerTintColor: "#000000",
-      headerBackTitle: "Back",
-      cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-      gestureEnabled: true,
-      gestureDirection: "horizontal",
-     }}
-    >
-     <Stack.Screen name="MemoList" component={MemoListScreen} />
-     <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
-     <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
-     <Stack.Screen name="ToDoList" component={ToDoScreen} />
-     <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
-     <Stack.Screen
-      name="Login"
-      component={LoginScreen}
-      options={{
-       cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+  <ApolloProvider client={apolloClient}>
+   <Provider store={store}>
+    <NavigationContainer>
+     <Stack.Navigator
+      initialRouteName="SignUp"
+      screenOptions={{
+       headerStyle: { backgroundColor: "#dffad4" },
+       headerTitleStyle: { color: "#000000" },
+       headerTitle: i18n.t("headerTitle"),
+       headerTintColor: "#000000",
+       headerBackTitle: "Back",
+       cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+       gestureEnabled: true,
+       gestureDirection: "horizontal",
       }}
-     />
-     <Stack.Screen
-      name="SignUp"
-      component={SignUpScreen}
-      options={{
-       cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
-      }}
-     />
-    </Stack.Navigator>
-   </NavigationContainer>
-  </Provider>
+     >
+      <Stack.Screen name="MemoList" component={MemoListScreen} />
+      <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
+      <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
+      <Stack.Screen name="ToDoList" component={ToDoScreen} />
+      <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
+      <Stack.Screen
+       name="Login"
+       component={LoginScreen}
+       options={{
+        cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+       }}
+      />
+      <Stack.Screen
+       name="SignUp"
+       component={SignUpScreen}
+       options={{
+        cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+       }}
+      />
+     </Stack.Navigator>
+    </NavigationContainer>
+   </Provider>
+  </ApolloProvider>
  );
 }
