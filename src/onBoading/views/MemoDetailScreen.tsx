f67d5be3398189b11ca6678510b052e989e845c6
Kouta Nikkuni <kota.nikkuni@linksports.co.jp>
2023-11-07 20:39:58 +0900

    change formatter

--- src/onBoading/views/MemoDetailScreen.tsx
+++ src/onBoading/views/MemoDetailScreen.tsx
@@ -5,58 +5,58 @@ import CircleButton from "../../components/CircleButton";
 import { shape } from "prop-types";
 
 export default function MemoListScreen(props) {
-  const { navigation } = props;
-  return (
-    <View>
-      <View style={styles.memoHeader}>
-        <Text style={styles.memoTitle}>買い物リスト</Text>
-        <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
-      </View>
-      <ScrollView style={styles.memoBody}>
-        <Text style={styles.memoText}>
-          買い物リスト 書体やレイアウトを確認 本文用
-        </Text>
-      </ScrollView>
-      <CircleButton
-        style={{ top: 60, bottom: "auto" }}
-        name="edit"
-        onPress={() => {
-          navigation.navigate("MemoEdit");
-        }}
-      ></CircleButton>
-    </View>
-  );
+ const { navigation } = props;
+ return (
+  <View>
+   <View style={styles.memoHeader}>
+    <Text style={styles.memoTitle}>買い物リスト</Text>
+    <Text style={styles.memoDate}>2020年12月24日 10:00</Text>
+   </View>
+   <ScrollView style={styles.memoBody}>
+    <Text style={styles.memoText}>
+     買い物リスト 書体やレイアウトを確認 本文用
+    </Text>
+   </ScrollView>
+   <CircleButton
+    style={{ top: 60, bottom: "auto" }}
+    name="edit"
+    onPress={() => {
+     navigation.navigate("MemoEdit");
+    }}
+   ></CircleButton>
+  </View>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F0F4F8",
-  },
-  memoHeader: {
-    backgroundColor: "#467FD3",
-    height: 96,
-    justifyContent: "center",
-    paddingVertical: 24,
-    paddingHorizontal: 19,
-  },
-  memoTitle: {
-    color: "#ffffff",
-    fontSize: 20,
-    lineHeight: 32,
-    fontWeight: "bold",
-  },
-  memoDate: {
-    color: "#ffffff",
-    fontSize: 12,
-    lineHeight: 16,
-  },
-  memoBody: {
-    paddingVertical: 32,
-    paddingHorizontal: 27,
-  },
-  memoText: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
+ container: {
+  flex: 1,
+  backgroundColor: "#F0F4F8",
+ },
+ memoHeader: {
+  backgroundColor: "#467FD3",
+  height: 96,
+  justifyContent: "center",
+  paddingVertical: 24,
+  paddingHorizontal: 19,
+ },
+ memoTitle: {
+  color: "#ffffff",
+  fontSize: 20,
+  lineHeight: 32,
+  fontWeight: "bold",
+ },
+ memoDate: {
+  color: "#ffffff",
+  fontSize: 12,
+  lineHeight: 16,
+ },
+ memoBody: {
+  paddingVertical: 32,
+  paddingHorizontal: 27,
+ },
+ memoText: {
+  fontSize: 16,
+  lineHeight: 24,
+ },
 });
--- src/onBoading/views/MemoEditScreen.tsx
+++ src/onBoading/views/MemoEditScreen.tsx
@@ -6,35 +6,36 @@ import CircleButton from "../../components/CircleButton";
 import KeyboardSafeView from "../../components/KeyBoardSafeView";
 
 export default function MemoEditScreen(props) {
-  const { navigation } = props;
-  return (
-    <KeyboardSafeView style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput value="買い物リスト" multiline style={styles.input} />
-      </View>
-      <CircleButton
-        name="check"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
-    </KeyboardSafeView>
-  );
+ const { navigation } = props;
+
+ return (
+  <KeyboardSafeView style={styles.container}>
+   <View style={styles.inputContainer}>
+    <TextInput value="買い物リスト" multiline style={styles.input} />
+   </View>
+   <CircleButton
+    name="check"
+    onPress={() => {
+     navigation.goBack();
+    }}
+   />
+  </KeyboardSafeView>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  inputContainer: {
-    paddingHorizontal: 27,
-    paddingVertical: 32,
-    flex: 1,
-  },
-  input: {
-    flex: 1,
-    textAlignVertical: "top",
-    fontSize: 16,
-    lineHeight: 24,
-  },
+ container: {
+  flex: 1,
+ },
+ inputContainer: {
+  paddingHorizontal: 27,
+  paddingVertical: 32,
+  flex: 1,
+ },
+ input: {
+  flex: 1,
+  textAlignVertical: "top",
+  fontSize: 16,
+  lineHeight: 24,
+ },
 });
